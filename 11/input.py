99fdeb3085c45dd59f83eafd0dc3232fa1fbb5f2
zwdnet <zwdnet@163.com>
2016-10-29 14:21:34 +0800

    finish execise 12

---
+++ 11/input.py
@@ -0,0 +1,7 @@
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How much do you weight")
+weight = input()
+print("So, you're %s old, %s tall and %s heavy." % (age, height, weight))
---
+++ 12/input2.py
@@ -0,0 +1,4 @@
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weight")
+print("So, you're %s old, %s tall and %s heavy." % (age, height, weight))
