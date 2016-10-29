5ffab79b3e986808fa966bde85d916bea9e45110
zwdnet <zwdnet@163.com>
2016-10-29 15:16:50 +0800

    finish execise 15

---
+++ 13/arg.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:%s" % script)
+print("first variable is:%s" % first)
+print("second variable is:%s" % second)
+print("third variable is:%s" % third)
